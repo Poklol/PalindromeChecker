cb22f0bb60787908c05de01e475360f2c12be788
Poklol <charukesh.baskaran@gmail.com>
2026-02-18 15:15:17 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -5,6 +5,31 @@ public class PalindromeChecker {
         System.out.println("===================================");
         System.out.println("Application Version: 1.0");
         System.out.println("Status: Application Started Successfully");
+
+
+        String input = "madam";
+
+        System.out.println("Checking Hardcoded String: " + input);
+
+        boolean isPalindrome = true;
+
+        // Compare characters from both ends
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display Result
+        if (isPalindrome) {
+            System.out.println("Result: The string is a Palindrome ");
+        } else {
+            System.out.println("Result: The string is NOT a Palindrome ");
+        }
+
+        System.out.println("\nProgram Ended Successfully.");
     }
 
 }
